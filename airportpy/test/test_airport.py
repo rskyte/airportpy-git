817a137f66b967956530c159bd397d0e148db285
Rhys Kyte <rskyte@gmail.com>
2019-06-03 13:31:56 +0100

    test suite setup first test failing

--- airportpy/test/test_airport.py
+++ airportpy/test/test_airport.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sys
-import unittest
 sys.path.append('./airportpy')
+
+import unittest
+from unittest.mock import Mock
 from src.airport import Airport
 
 FAILURE = 'incorrect value'
@@ -9,6 +11,13 @@ FAILURE = 'incorrect value'
 class AirportTest(unittest.TestCase):
     def setUp(self):
         self.airport = Airport()
+        self.plane = Mock()
+
+    def test_airport_can_land_planes(self):
+        self.airport.land(self.plane)
+        value = self.airport.plane
+        self.assertEqual(value, self.plane, "plane has not landed")
+    
 
 
 if __name__ == '__main__':
